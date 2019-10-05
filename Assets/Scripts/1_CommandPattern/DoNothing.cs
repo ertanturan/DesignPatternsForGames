e5c4d83f660d053d7d26456c2032239b34edec45
Ertan Turan <ertanturan@outlook.com>
2019-10-05 23:46:40 +0300

    Abstract classes created and basic inheritation set.

---
+++ Assets/Scripts/1_CommandPattern/DoNothing.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNothing : Command
+{
+    public override void Execute(Animator anim)
+    {
+
+    }
+}
