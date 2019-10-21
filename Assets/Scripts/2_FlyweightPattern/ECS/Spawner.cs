50addaa09bb6a40c1addd8cc4fd41513850e7b38
Ertan Turan <ertanturan@outlook.com>
2019-10-21 11:20:56 +0300

    Basic perlin spawner created and scene adjusted

---
+++ Assets/Scripts/2_FlyweightPattern/ECS/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Cube;
+    public int Rows;
+    public int Cols;
+
+    private void Start()
+    {
+        for (int x = 0; x < Rows; x++)
+        {
+            for (int z = 0; z < Cols; z++)
+            {
+                Vector3 pos = new Vector3(x,
+                    Mathf.PerlinNoise(x * .21f, z * .21f)
+                    , z);
+                GameObject.Instantiate(Cube, pos, Quaternion.identity);
+            }
+        }
+    }
+}
