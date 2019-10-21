8b0b8f94849f82de3b55481425731beb0e54ee0d
Ertan Turan <ertanturan@outlook.com>
2019-10-21 11:42:45 +0300

    ecs part 1

---
+++ Assets/Scripts/2_FlyweightPattern/ECS/SpawnCar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCar : MonoBehaviour
+{
+    public GameObject Car;
+
+    public GameObject Camera;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 pos = new Vector3(10,10,10);
+            GameObject c = Instantiate(Car, pos, Quaternion.identity);
+            Camera.GetComponent<SmoothFollow>().target = c.transform;
+        }
+    }
+
+}
