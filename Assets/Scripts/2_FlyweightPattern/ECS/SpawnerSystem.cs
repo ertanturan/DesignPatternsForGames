cb79d5251aac862938372981c8e107009adf876a
Ertan Turan <ertanturan@outlook.com>
2019-10-22 10:22:58 +0300

    spawner system developed.

--- Assets/Scripts/2_FlyweightPattern/ECS/SpawnerSystem.cs
+++ Assets/Scripts/2_FlyweightPattern/ECS/SpawnerSystem.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
 
-public class SpawnerSystem : MonoBehaviour
+
+public class SpawnerSystem : JobComponentSystem
 {
-    // Start is called before the first frame update
-    void Start()
+    private EndSimulationEntityCommandBufferSystem m_EntityCommandBufferSystem;
+
+    protected override void OnCreateManager()
     {
-        
+        m_EntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
     }
 
-    // Update is called once per frame
-    void Update()
+    struct SpawnJob : IJobForEachWithEntity<Spawner, LocalToWorld>
     {
-        
+        public EntityCommandBuffer CommandBuffer;
+
+        public void Execute(Entity entity, int index, [ReadOnly] ref Spawner spawner,
+            [ReadOnly] ref LocalToWorld location)
+        {
+            for (int x = 0; x < spawner.ERows; x++)
+            {
+                for (int z = 0; z < spawner.ECols; z++)
+                {
+                    var instance = CommandBuffer.Instantiate(spawner.Prefab);
+                    var pos = math.transform(location.Value,
+                        new float3(
+                            x, noise.cnoise(new float2(x, z) * .21f), z
+                        )
+                    );
+
+                    CommandBuffer.SetComponent(instance, new Translation { Value = pos });
+                }
+            }
+            CommandBuffer.DestroyEntity(entity);
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+
+        var job = new SpawnJob
+        {
+            CommandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer()
+
+        }.ScheduleSingle(this, inputDeps);
+
+        m_EntityCommandBufferSystem.AddJobHandleForProducer(job);
+
+        return job;
     }
 }
