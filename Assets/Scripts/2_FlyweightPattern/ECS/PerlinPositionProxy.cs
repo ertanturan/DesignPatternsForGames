20cfa9422b90645e4bcf2006bc8f31b51fa941c1
Ertan Turan <ertanturan@outlook.com>
2019-10-22 09:31:33 +0300

    ECS Codebase set .

---
+++ Assets/Scripts/2_FlyweightPattern/ECS/PerlinPositionProxy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+
+[RequiresEntityConversion]
+public class PerlinPositionProxy : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager manager,
+        GameObjectConversionSystem conversionSystem)
+    {
+
+        PerlinPosition data = new PerlinPosition { };
+
+        manager.AddComponentData(entity, data);
+
+    }
+
+}
---
+++ Assets/Scripts/2_FlyweightPattern/ECS/SpawnProxy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class SpawnProxy : MonoBehaviour, IDeclareReferencedPrefabs, IConvertGameObjectToEntity
+{
+
+    public GameObject Cube;
+    public int Rows;
+    public int Cols;
+
+    public void DeclareReferencedPrefabs(List<GameObject> list)
+    {
+        list.Add(Cube);
+    }
+
+    public void Convert(Entity entity, EntityManager manager,
+        GameObjectConversionSystem conversionSystem)
+    {
+        var spawnerData = new Spawner
+        {
+            Prefab = conversionSystem.GetPrimaryEntity(Cube),
+            ERows = Rows,
+            ECols = Cols,
+        };
+            manager.AddComponentData(entity,spawnerData);
+
+    }
+
+
+}
+
+
