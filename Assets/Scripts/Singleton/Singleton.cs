9600fcd50a32b8a3596ec3437c533cf3ca6f9353
Ertan Turan <ertanturan@outlook.com>
2019-12-26 09:33:56 +0300

    Singleton pattern added

---
+++ Assets/Scripts/Singleton/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    public bool DontDestroyOnLoading = false;
+
+    public T Instance
+    {
+        get
+        {
+
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).Name);
+                    obj.AddComponent<T>();
+                }
+            }
+
+            return _instance;
+
+        }
+    }
+
+    private T _instance;
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            if (DontDestroyOnLoading)
+                DontDestroyOnLoad(gameObject);
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
